0c87c52a27046096750f77f674d1e70fc1deedb9
Yuliia Romanovych <jdani22101998@gmail.com>
2023-04-28 20:51:57 +0300

    Виконання 1-го завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const galleryListItem = galleryItems.map(item => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+         <img
+         class="gallery__image"
+         src="${item.preview}" 
+         alt="${item.description}"
+         />
+      </a>
+    </li>`;
+  }).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryListItem);
+
+const imgCaption = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250
+});
