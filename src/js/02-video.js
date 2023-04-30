14cb21ac47f2f6345c7e8f76be763816d8ff7ca3
Yuliia Romanovych <jdani22101998@gmail.com>
2023-04-30 22:24:07 +0300

    Виконання 2-го завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    const currentTime = time.seconds;
+    localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
