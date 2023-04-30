fac61d0336a4ec92ada5da76f47b798043872c47
Yuliia Romanovych <jdani22101998@gmail.com>
2023-04-30 22:29:19 +0300

    Виконання 3-го завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function saveFormState(e) {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+const throttleInput = throttle(saveFormState, 500);
+
+emailInput.addEventListener('input', throttleInput);
+messageInput.addEventListener('input', throttleInput);
+form.addEventListener('submit', formSubmit);
+
+saveInputToStorage();
+
+function saveInputToStorage() {
+  const savedValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedValue) {
+    emailInput.value = savedValue.email;
+    messageInput.value = savedValue.message;
+  }
+};
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  const mail = emailInput.value;
+  const message = messageInput.value;
+
+  if (mail !== '' && message !== '') {
+    const formData = {
+      email: mail,
+      message: message,
+    };
+    console.log(formData);
+
+    form.reset();
+  } else {
+    alert("Поля 'Email' або 'Message' не можуть бути пустими.");
+  }
+  localStorage.removeItem(STORAGE_KEY);
+};
